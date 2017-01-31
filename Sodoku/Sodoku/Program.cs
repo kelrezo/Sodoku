45ffb008345e09a4bbe814d70561d371c3f6815c
kelrezo <travis_layne@yahoo.com>
2017-01-30 22:43:27 -0600

    dfsdfsdf

--- Sodoku/Sodoku/Program.cs
+++ Sodoku/Sodoku/Program.cs
@@ -43,5 +43,13 @@ namespace Sodoku
             }
             return false;
         }
+        bool FindUnassignedLocation(int[,] grid, int row, int col)
+        {
+            for (row = 0; row < 9; row++)
+                for (col = 0; col < 9; col++)
+                    if (grid[row,col] == 0)
+                        return true;
+            return false;
+        }
     }
 }
